25805137183549b881fb7d763c2174f12a8d9e78
DianQK <dianqk@icloud.com>
2017-11-05 11:47:12 +0800

    Fix typo

--- Flix/Flix.h
+++ Flix/Flix.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 DianQK. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 //! Project version number for Flix.
 FOUNDATION_EXPORT double FlixVersionNumber;
 
